78532e6ff37e7b1a31ee2bf5fa5a3ca2f6a672ab
znehraks <znehraks@gmail.com>
2024-11-01 21:16:22 +0900

    feat: checkbox component 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Typography } from './components/common/Typography';
 import { Badge } from './components/common/Badge';
+import { Checkbox } from './components/common/Checkbox';
 
 function App() {
   const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ function App() {
       <Typography color={'brand'}>Hello, world!</Typography>
       <Badge variant={'brand'}>시</Badge>
       <Badge variant={'default'}>끝</Badge>
+      <Checkbox checked />
       <div className={'w-screen h-screen bg-green-100'}>hi</div>
     </QueryClientProvider>
   );
---
+++ src/components/common/Checkbox.tsx
@@ -0,0 +1,26 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import { forwardRef } from 'react';
+import { cn } from '../../utils';
+
+const checkboxVariants = cva('', {
+  variants: {
+    size: {
+      medium: 'w-4 h-4',
+    },
+  },
+  defaultVariants: {
+    size: 'medium',
+  },
+});
+
+export type CheckboxProps = VariantProps<typeof checkboxVariants> &
+  React.InputHTMLAttributes<HTMLInputElement> & {
+    className?: string;
+  };
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
+  const { size = 'medium', ...rest } = props;
+  return <input ref={ref} type="checkbox" className={cn(checkboxVariants({ size }), props.className)} {...rest} />;
+});
+
+Checkbox.displayName = 'Checkbox';
