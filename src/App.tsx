a5bfac8c391fefcefdb1da46d9759407eff0db8a
znehraks <znehraks@gmail.com>
2024-11-01 21:36:46 +0900

    feat: Icon Button Component 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,28 @@ import { Typography } from './components/common/Typography';
 import { Badge } from './components/common/Badge';
 import { Checkbox } from './components/common/Checkbox';
 import { IconButton } from './components/common/IconButton';
+import { Tag } from './components/common/Tag';
+import { ChevronLeft } from 'lucide-react';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Typography color={'brand'}>Hello, world!</Typography>
-      <Badge variant={'brand'}>시</Badge>
-      <Badge variant={'default'} size={'small'}>
-        끝
-      </Badge>
-      <IconButton size={'small'}>
-        <div>{'>'}</div>
-      </IconButton>
-      <Checkbox checked />
-      <div className={'w-screen h-screen bg-green-100'}>hi</div>
+      <div className={'w-screen h-screen bg-green-100 '}>
+        <Typography color={'brand'}>Hello, world!</Typography>
+        <Badge variant={'brand'}>시</Badge>
+        <Badge variant={'default'} size={'small'}>
+          끝
+        </Badge>
+        <IconButton>
+          <ChevronLeft size={16} />
+        </IconButton>
+        <Checkbox checked />{' '}
+        <Tag variant={'default'} size={'medium'}>
+          경영기획
+        </Tag>
+      </div>
     </QueryClientProvider>
   );
 }
--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -1,28 +1,14 @@
-import { cva, VariantProps } from 'class-variance-authority';
 import { forwardRef } from 'react';
-import { cn } from '../../utils';
 
-const IconButtonVariants = cva('', {
-  variants: {
-    size: {
-      small: 'w-6 h-6',
-    },
-  },
-  defaultVariants: {
-    size: 'small',
-  },
-});
-
-export type IconButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
-  VariantProps<typeof IconButtonVariants> & {
-    className?: string;
-  };
+export type IconButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  className?: string;
+};
 
 export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
   const { className, children, ...rest } = props;
 
   return (
-    <button ref={ref} className={cn(IconButtonVariants({ size: 'small' }), className)} {...rest}>
+    <button ref={ref} className={className} {...rest}>
       {children}
     </button>
   );
