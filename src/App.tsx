68d56cacaa843d9332da322138e3b975231ac941
znehraks <znehraks@gmail.com>
2024-11-01 19:30:51 +0900

    chore: common components file and calendar page components settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 function App() {
-  return <div className={'bg-red-500'}>hi</div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className={'w-screen h-screen bg-green-100'}>hi</div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
