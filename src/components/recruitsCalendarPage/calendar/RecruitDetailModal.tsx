89a2d86d29ab77ea8e8360551c3fc79afe490306
znehraks <znehraks@gmail.com>
2024-11-04 00:10:35 +0900

    fix: modal click event handler 위치 변경

--- src/components/recruitsCalendarPage/calendar/RecruitDetailModal.tsx
+++ src/components/recruitsCalendarPage/calendar/RecruitDetailModal.tsx
@@ -55,36 +55,31 @@ export function RecruitDetailModal({
   if (detailRecruitInfo === null) return null;
 
   return createPortal(
-    <>
-      <Backdrop />
-      <div
-        className=" fixed top-0 left-0 right-0 bottom-0 w-screen h-screen flex justify-center items-center "
-        onClick={handleClose}
-      >
-        <div className="relative min-w-[750px] w-4/5 h-screen flex flex-col  bg-white rounded-md overflow-auto">
-          <div className="absolute top-3 right-5">
-            <X size={20} className="text-gray-500 cursor-pointer" onClick={handleClose} />
-          </div>
-          <div className="p-9 pb-0 flex flex-col gap-2">
-            <Typography variant="content">{detailRecruitInfo.company_name}</Typography>
-            <Typography variant="title" color="gray">
-              {detailRecruitInfo.title}
+    <div className=" fixed top-0 left-0 right-0 bottom-0 w-screen h-screen flex justify-center items-center ">
+      <Backdrop onClick={handleClose} />
+      <div className="relative min-w-[750px] w-4/5 h-screen flex flex-col  bg-white rounded-md overflow-auto">
+        <div className="absolute top-3 right-5">
+          <X size={20} className="text-gray-500 cursor-pointer" onClick={handleClose} />
+        </div>
+        <div className="p-9 pb-0 flex flex-col gap-2">
+          <Typography variant="content">{detailRecruitInfo.company_name}</Typography>
+          <Typography variant="title" color="gray">
+            {detailRecruitInfo.title}
+          </Typography>
+          <div className="flex items-center gap-1">
+            <Typography variant="detail" color="gray">
+              {detailRecruitInfo.start_time} ~ {detailRecruitInfo.end_time}
             </Typography>
-            <div className="flex items-center gap-1">
-              <Typography variant="detail" color="gray">
-                {detailRecruitInfo.start_time} ~ {detailRecruitInfo.end_time}
-              </Typography>
-              <Typography className="text-red-400">{`(${dayjs(detailRecruitInfo.end_time).diff(dayjs(), 'day')}일 남음)`}</Typography>
-            </div>
+            <Typography className="text-red-400">{`(${dayjs(detailRecruitInfo.end_time).diff(dayjs(), 'day')}일 남음)`}</Typography>
           </div>
-          <div className="p-9 min-h-fit">
-            <img src={detailRecruitInfo.image_url} alt="recruit" className="w-full h-96 object-cover rounded-md" />
-          </div>
+        </div>
+        <div className="p-9 min-h-fit">
+          <img src={detailRecruitInfo.image_url} alt="recruit" className="w-full h-96 object-cover rounded-md" />
         </div>
       </div>
       <ChevronLeft className="fixed left-0 top-1/2 text-white cursor-pointer" size={64} onClick={handlePrevClick} />
       <ChevronRight className="fixed right-0 top-1/2 text-white cursor-pointer" size={64} onClick={handleNextClick} />
-    </>,
+    </div>,
     document.body,
   );
 }
